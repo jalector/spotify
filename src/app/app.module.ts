0103ade450491638ccbbf058b75e118616f8ca9b
Juda Alector <juda_alector@hotmail.com>
2018-08-22 21:22:39 -0500

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
+//Providers
+import { SpotifyService } from './services/spotify.service';
+
+//Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
+
+//Routes
+import { ROUTES } from './app.routes';
+import { NoimagePipe } from './pipes/noimage.pipe';
+import { CardsComponent } from './components/share/cards/cards.component';
+import { LoadingComponent } from './components/share/loading/loading.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent,
+    NoimagePipe,
+    CardsComponent,
+    LoadingComponent        
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, 
+    RouterModule.forRoot( ROUTES,{ useHash:true } )
   ],
-  providers: [],
+  providers: [ SpotifyService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
